59462d8cecb14943d9740c7e01c4f14adf63480d
ThanhSang <thanhsang6325@gmail.com>
2023-06-16 14:12:03 +0700

    fix balance overview

--- src/components/client/BalanceOverview.vue
+++ src/components/client/BalanceOverview.vue
@@ -2,21 +2,21 @@
   <div class="balance-overview">
     <div class="balance-wrapper">
       <span>Số dư tài khoản</span>
-      <h3>0đ</h3>
+      <h3>{{ toVnd(user.balance) }}</h3>
     </div>
-    <router-link to="/">
+    <router-link to="/payment-history">
       <button class="link">
         <span>Lịch sử nạp tiền</span>
         <i class="bx bx-chevron-right"></i>
       </button>
     </router-link>
-    <router-link to="/">
+    <router-link to="/purchase-history">
       <button class="link">
         <span>Lịch sử thanh toán</span>
         <i class="bx bx-chevron-right"></i>
       </button>
     </router-link>
-    <router-link to="/">
+    <router-link to="/price">
       <button class="link">
         <span>Bảng giá dịch vụ</span>
         <i class="bx bx-chevron-right"></i>
@@ -26,7 +26,16 @@
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+import { toVnd } from '@/utils/numberFormatter';
+export default {
+  computed: {
+    ...mapState('client', ['user']),
+  },
+  methods: {
+    toVnd,
+  },
+};
 </script>
 
 <style scoped src="@/assets/styles/client/balance_overview.css"></style>
